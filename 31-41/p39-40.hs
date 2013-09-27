b8aa2aabab2dd24f5ecaddd7ab141fa0eff842c8
leeTheWHUs <lrlanswer@gmail.com>
2013-09-27 20:31:45 +0800

    solve a bug on the primes problems

--- 31-41/p39-40.hs
+++ 31-41/p39-40.hs
@@ -1,14 +1,12 @@
 primesR :: (Integral a) => a -> a -> [a]
-primesR min max
-		| even min = filter isPrime [min+1, min+3..max]
-		| otherwise = filter isPrime [min, min+2..max]
+primesR min max = filter isPrime [min..max]
 
 
 isPrime :: (Integral a) => a -> Bool
-isPrime n | n <4 = n >1
-isPrime n = all ((/= 0). mod n) $ 2:3:[x+i | x <- [6,12..s], i <- [-1,1]]
-		where s = floor . sqrt $ fromIntegral n
+isPrime n | n < 4 = n > 1
+isPrime n = all ((/=0).mod n) $ [x | x <- [2..s]]
+            where s = floor $ sqrt $ fromIntegral n
 
 goldbach :: (Integral a) => a -> [(a,a)]
 goldbach n = [(x,y)| x <- primesR 2 n, let y=n-x, isPrime (y)]
-		
+        
